422c85c7f7d8a98ecb88f5e0ffeba8aa911bcdda
morozMyk <mik.moroz0387@gmail.com>
2024-09-03 12:26:08 +0300

    created table, added filters and sorting features

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { useState } from 'react'
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { FilterMatchMode } from 'primereact/api';
+import { InputText } from 'primereact/inputtext';
+import { useState } from 'react';
 
 type UsersProps = {
   users: {
@@ -10,54 +14,40 @@ type UsersProps = {
     phone: string
   }[];
 }
-type UserProps = {
-  user: {
-    id: number,
-    name: string, 
-    username: string, 
-    email: string,  
-    phone: string
-  }
-}
 
 function Users({users}:UsersProps) {
   
-
-  const renderUsers = users.map(user => {
-    return (
-      <User user={user}/>
-    )
+  const [filter, setFilter] = useState({
+    id: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    username: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    email: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    phone: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
   })
 
-  
-  function User({user}:UserProps){
-    const [accordionOpen, setAccordionOpen] = useState(false); 
-
-    return (
-      <div className='bg-slate-900 text-white p-4 my-6 w-1/2'>
-        <div className='flex justify-between'>
-          <div className=''>{user.username}</div>
-          <button
-            onClick={() => setAccordionOpen(!accordionOpen)}
-            className=''>
-              Show more 
-          </button>
-        </div>
-        <div className={`transition-all duration-300 ease-in-out text-white text-sm ${
-          accordionOpen
-            ? ``
-            : "hidden"
-        }`}id="collapse" data-twe-collapse-item>
-          <div className='flex-1 w-fit'>Name: {user.name}</div>
-          <div className='flex-1 w-fit'>Email: {user.email}</div>
-          <div className='flex-1 w-fit'>Phone number: {user.phone}</div>
-        </div>
-      </div>
-    )
-  }
   return (
     <>
-     {renderUsers}
+      {/* <InputText className='m-4 bg-slate-500 text-white' onInput={(e)=>{
+        setFilter({
+          global: {value: e.target.value, matchMode: FilterMatchMode.CONTAINS}
+        })
+      }}/> */}
+     <DataTable value={users} stripedRows dataKey="id" filters={filter} filterDisplay="row" emptyMessage="No customers found."
+      globalFilterFields={['id','username', 'name', 'email', 'phone']} size='large' 
+      >
+
+      <Column field='id' header='ID'sortable filter filterPlaceholder="Search by id" 
+        className='border-b border-black p-2'/>
+      <Column field='username' header='Username' sortable filter filterPlaceholder="Search by name"
+        className='border-b border-black'/>
+      <Column field='name' header='Name' sortable filter filterPlaceholder="Search by username" 
+        className='border-b border-black'/>
+      <Column field='email' header='Email' sortable filter filterPlaceholder="Search by email"
+        className='border-b border-black'/>
+      <Column field='phone' header='Phone number' sortable filter filterPlaceholder="Search by phone number"
+        className='border-b border-black'/>
+
+     </DataTable>
     </>
   )
   
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { PrimeReactProvider } from "primereact/api";
+import Tailwind from 'primereact/passthrough/tailwind';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PrimeReactProvider value={{ unstyled: true, pt: Tailwind  }}>
+        <App />
+      </PrimeReactProvider>
     </Provider>
   </React.StrictMode>,
 )
