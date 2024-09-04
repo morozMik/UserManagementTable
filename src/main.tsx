b1670578f30b757a51f51c439772f5c25dd34893
morozMyk <mik.moroz0387@gmail.com>
2024-09-04 15:56:50 +0300

    Title change

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { PrimeReactProvider } from "primereact/api";
-import Tailwind from 'primereact/passthrough/tailwind';
 import { MyDesignSystem } from './CustomTailwindTableComfig.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
