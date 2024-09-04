7266c1858b6b1b27c5f6ebdb5ffb9ce14e3e6f81
morozMyk <mik.moroz0387@gmail.com>
2024-09-04 16:08:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/UserManagementTable/",
+  base: "https://morozmik.github.io/UserManagementTable/",
   plugins: [react()],
 })
